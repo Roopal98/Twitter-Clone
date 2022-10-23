8298616c35d61b7a94eb5432f03fe4acb3955673
Roopal98 <roopal.dumka@gmail.com>
2022-10-23 15:18:34 +0530

    edited css

--- src/components/Right/RightPanel.js
+++ src/components/Right/RightPanel.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import SearchIcon from "@mui/icons-material/Search";
-// import { TwitterTimelineEmbed, TwitterTweetEmbed } from "react-twitter-embed";
 import "./RightPanel.css";
 import { useSelector } from "react-redux";
 import { ListGroup, ListGroupItem } from "react-bootstrap";
@@ -10,15 +9,14 @@ const RightPanel = () => {
   const allTweets = useSelector((state)=> state.tweet.tweets)
 
   useEffect(()=>{
-    // console.log(`filter ${filter}`)
     if(filter.length===0) {
       setFilteredTweets([]);
-      console.log(`filter ${filter}`)
+      // console.log(`filter ${filter}`)
        return;}
     const fetchData =  () => {
       
         const filteredData = allTweets.filter(obj => obj.text.match(filter))
-        console.log(filteredData)
+        // console.log(filteredData)
         setFilteredTweets(filteredData)
       }
     
@@ -35,17 +33,7 @@ const RightPanel = () => {
         <input placeholder="Search Twitter" value={filter} type="text" onChange={(e)=>setFilter(e.target.value)}/>
       </div>
       <div className="widgets__widgetContainer">
-        <h5>What's Happening</h5>
-        {/* <TwitterTweetEmbed
-          tweetId={"1189966818640900096"}
-          options={{ height: 350 }}
-        /> */}
-        {/* <TwitterTimelineEmbed
-          sourceType="profile"
-          screenName="reactjs"
-          noScrollbar
-          style={{ width: "100%" }}
-        /> */}
+        <h5>What's Happening?</h5>
         <ListGroup variant="flush">
           {filteredTweets.length>0 &&  filteredTweets.map((obj)=>
             <ListGroupItem key={obj.author_id}>
